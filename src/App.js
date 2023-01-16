ac34c42bc8b6e92e15d18072c74306d5dc24a13d
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2023-01-16 13:56:24 +0100

    Fixed previous commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-// Import everything needed to use the `useQuery` hook
-import { useQuery, gql } from '@apollo/client';
+import logo from './components/RaMlogo.png';
+import Header from './components/header';
+import Title from './components/title';
+import Footer from './components/footer';
+import './components/styles.css'
 
-
-const GET_LOCATIONS = gql`
-  query GetLocations{
-    results {
-      gender
-    }
-  }
-`;
-
-function DisplayLocations() {
-  const {loading, error,data } = useQuery(GET_LOCATIONS);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-
-  return data.info.map(({gender}) => (
-    <div >
-      <p>{gender}</p>
-    </div>
-  ));
-}
-
-export default function episodesList() {
+function App() {
   return (
     <div>
-      <h2>My first Apollo 🚀</h2>
-      <br/>
-      <DisplayLocations />
+        <Header />
+        <Title />
+        <img className = "logo" src = {logo} />
+        <Footer />
     </div>
   );
 }
+
+export default App;
