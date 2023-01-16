fbaf515620d4ba300a0f515e543714f0b0077482
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2023-01-16 12:26:48 +0100

    Added title, footer, logo
    Not sure about position of footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./styles.css";
+
+class Footer extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <object className="footer" />
+        <h1 className="footerText"> LOREM IPSUM ©2021</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
