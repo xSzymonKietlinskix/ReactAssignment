abd14735d8a7ccbd66a0bc8d7e93dc59417c73e4
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2023-01-16 13:56:24 +0100

    Fixed previous commit

--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 import './components/styles.css'
-
-const client = new ApolloClient({
-  uri: "https://api.randomuser.me/",
-  cache: new InMemoryCache(),
-});
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <ApolloProvider client={client}>
+  <React.StrictMode>
     <App />
-  </ApolloProvider>,
+  </React.StrictMode>
 );
+
 
